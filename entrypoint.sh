57af01dc949696945e80be5e6a34d9162a1a6e60
Paul A.T. Wilson <paul.wilson@energylinx.co.uk>
2020-05-03 19:44:07 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+aws configure set region eu-west-2 --profile dev-deploy
+aws configure set profile.dev-deploy.aws_access_key_id "${AWS-ACCESS-KEY-ID}"
+aws configure set profile.dev-deploy.aws_secret_access_key "${AWS-SECRET-ACCESS-KEY}"
+npm i -g serverless
+serverless deploy --stage dev
